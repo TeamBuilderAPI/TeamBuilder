848991ef2621e983f337b99a3878d39ca34deeee
TeamBuilderAPI <dungeoncrawler101@outlook.com>
2024-03-15 21:02:00 -0400

    Added the Team Builder field model. Static readonly values that will be supplied upon reading in init config values. Also added a demo page for ideas, thanks to the WeatherForcast demo example! Next Steps begin feature branches.

---
+++ TeamBuilder/Model/FieldModel.cs
@@ -0,0 +1,17 @@
+﻿namespace TeamBuilder.Model
+{
+    public static class FieldModel
+    {
+        private static readonly string _configKey1;
+        static FieldModel()
+        {
+            //Todo add and read in any global config key values here.
+            _configKey1 = "global config value";
+        }
+
+        public static string ConfigKey1
+        {
+            get { return _configKey1; }
+        }
+    }
+}
