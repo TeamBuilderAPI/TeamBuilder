15b07786e39aa9f8f54eabf56b9e8dc57eab5117
Christopher W. Peterson <dungeoncrawlerdaddy@outlook.com>
2024-04-10 18:06:07 -0400

    Code cleanup profile run. Added a DTO class for TodoItem. Data Transfer Object (DTO), input model, or view model. DTO is used in this tutorial: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&tabs=visual-studio
    "A DTO may be used to:
    Prevent over-posting.
    Hide properties that clients are not supposed to view.
    Omit some properties in order to reduce payload size.
    Flatten object graphs that contain nested objects. Flattened object graphs can be more convenient for clients.".

---
+++ TeamBuilder/Models/TodoItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TeamBuilder.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
