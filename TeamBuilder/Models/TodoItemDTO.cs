2b874ecd4efe63c9cd1497ba3bc0226702507056
Christopher W. Peterson <dungeoncrawlerdaddy@outlook.com>
2024-04-10 18:45:45 -0400

    comment

--- TeamBuilder/Models/TodoItemDTO.cs
+++ TeamBuilder/Models/TodoItemDTO.cs
@@ -1,5 +1,14 @@
 ﻿namespace TeamBuilder.Models
 {
+    /* 
+    Added a Data Transfer Object (DTO)!     
+    "A DTO may be used to:
+    Prevent over-posting.
+    Hide properties that clients are not supposed to view.
+    Omit some properties in order to reduce payload size.
+    Flatten object graphs that contain nested objects. Flattened object graphs can be more convenient for clients.".
+    Ref: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&tabs=visual-studio
+    */
     public class TodoItemDTO
     {
         public long Id { get; set; }
