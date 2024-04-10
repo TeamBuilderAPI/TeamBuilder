66871c9c71f1601b89c8943ad51e5e0622aaec47
Christopher W. Peterson <dungeoncrawlerdaddy@outlook.com>
2024-04-10 19:46:46 -0400

    Encapsulate the DTO and use Object Oriented inheritence to avoid duplication of the same code. Still hides the properties from the UI this way.

--- TeamBuilder/Controllers/TodoItemsController.cs
+++ TeamBuilder/Controllers/TodoItemsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TeamBuilder.Models;
 
@@ -22,14 +17,17 @@ namespace TeamBuilder.Controllers
 
         // GET: api/TodoItems
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
         {
-            return await _context.TodoItems.ToListAsync();
+            return await _context.TodoItems
+                .Select(x => TodoItem.ItemToDTO(x))
+                .ToListAsync();
         }
 
         // GET: api/TodoItems/5
+        // <snippet_GetByID>
         [HttpGet("{id}")]
-        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
         {
             var todoItem = await _context.TodoItems.FindAsync(id);
 
@@ -38,51 +36,64 @@ namespace TeamBuilder.Controllers
                 return NotFound();
             }
 
-            return todoItem;
+            return TodoItem.ItemToDTO(todoItem);
         }
+        // </snippet_GetByID>
 
         // PUT: api/TodoItems/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // <snippet_Update>
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        public async Task<IActionResult> PutTodoItem(long id, TodoItemDTO todoDTO)
         {
-            if (id != todoItem.Id)
+            if (id != todoDTO.Id)
             {
                 return BadRequest();
             }
 
-            _context.Entry(todoItem).State = EntityState.Modified;
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            todoItem.Name = todoDTO.Name;
+            todoItem.IsComplete = todoDTO.IsComplete;
 
             try
             {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
             {
-                if (!TodoItemExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return NotFound();
             }
 
             return NoContent();
         }
+        // </snippet_Update>
 
         // POST: api/TodoItems
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // <snippet_Create>
         [HttpPost]
-        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        public async Task<ActionResult<TodoItemDTO>> PostTodoItem(TodoItemDTO todoDTO)
         {
+            var todoItem = new TodoItem
+            {
+                IsComplete = todoDTO.IsComplete,
+                Name = todoDTO.Name
+            };
+
             _context.TodoItems.Add(todoItem);
             await _context.SaveChangesAsync();
 
-            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(
+                nameof(GetTodoItem),
+                new { id = todoItem.Id },
+                TodoItem.ItemToDTO(todoItem));
         }
+        // </snippet_Create>
 
         // DELETE: api/TodoItems/5
         [HttpDelete("{id}")]
--- TeamBuilder/Models/TodoItem.cs
+++ TeamBuilder/Models/TodoItem.cs
@@ -1,9 +1,32 @@
 ﻿namespace TeamBuilder.Models
 {
-    public class TodoItem
+    /* 
+    Added a Data Transfer Object (DTO)!     
+    "A DTO may be used to:
+    Prevent over-posting.
+    Hide properties that clients are not supposed to view.
+    Omit some properties in order to reduce payload size.
+    Flatten object graphs that contain nested objects. Flattened object graphs can be more convenient for clients.".
+    Ref: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&tabs=visual-studio
+     */
+
+    public class TodoItemDTO
     {
         public long Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+    }
+
+    public class TodoItem : TodoItemDTO
+    {
+        public string? Secret { get; set; }
+
+        public static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
+            new TodoItemDTO
+            {
+            Id = todoItem.Id,
+            Name = todoItem.Name,
+            IsComplete = todoItem.IsComplete
+            };
     }
 }
